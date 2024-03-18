938819038971e78ee59a71fd0c93ac838493ca45
DivijJaswal <ellytopas@gmail.com>
2024-03-18 14:33:18 +0530

    added user model for reference

---
+++ Server/userModel.py
@@ -0,0 +1,131 @@
+
+#data types mentioned nahi hai kyonki sab string main store hoga python main waise bhi
+#json ka data type set karke abhipsa bhejegi hum sidha sidha store kardenge python khud figure out karlega list hai ya nahi types
+
+user_data = {
+            'name': name,
+            'email': email,
+            'alternate_email': alternate_email,
+            'password': hashed_password,
+            'verified': False ,
+            'contact': phone_number, #can be more than one
+            'gender': gender ,
+            'father_name': father_name,
+            'mother_name': mother_name,
+            'date_of_birth': date_of_birth,
+            'marital_status': marital_status,
+            'category': category,
+            'nationality': nationality,
+            'disability_status': disability_status,
+            'current_address': current_address,
+            'city': city,
+            'state': state,
+            'pin': pin,
+            'type_of_identity_proof': type_of_identity_proof,
+            'identity_card_no': identity_card_no,
+            'post_applying_for': post_applying_for,
+            'from_industry': from_industry,
+            'status_of_phd_degree': status_of_phd_degree,
+            'details_of_sponsored_projects': details_of_sponsored_projects, #list of objects
+            ## object
+            # sponsored_project={
+            #     'title': title,
+            #     'amount': amount,
+            #     'role': role,
+            #     'no_of_co-pi': no_of_co-pi,
+            #     'funding_agency': funding_agency,
+            #     'sanctioned_date': date,
+            #     'status': status
+            # }
+            'details_of_consultancy_projects': details_of_consultancy_projects, #list of objects
+            ## object
+            # consultancy_project={
+            #     'title': title,
+            #     'amount': amount,
+            #     'role': role,
+            #     'no_of_co-pi': no_of_co-pi,
+            #     'funding_agency': funding_agency,
+            #     'sanctioned_date': date,
+            #     'status': status
+            # }
+            'details_of_outreach_programs': details_of_outreach_programs, #list of objects
+            ## object
+            # outreach_programs={
+            #     'title': title,
+            #     'amount': amount,
+            #     'role': role,
+            #     'no_of_co-pi': no_of_co-pi,
+            #     'funding_agency': funding_agency,
+            #     'sanctioned_date': date,
+            #     'status': status
+            # }
+            'details_of_phd_supervised': details_of_phd_supervised, #list of objects
+            ## object
+            # phd_supervised={
+            #     'sole_guide': sole_guide,
+            #     'joint_guide': joint_guide
+            # }
+            'details_of_phd_supervising': details_of_phd_supervising, #list of objects
+            ## object
+            # phd_supervising={
+            #     'sole_guide': sole_guide,
+            # }
+
+            # page 3 ka info add hoga
+            
+
+
+            'details_of_sponsored_projects': details_of_sponsored_projects, #list of objects
+            ## object
+            # sponsored_project={
+            #     'title': title,
+            #     'amount': amount,
+            #     'role': role,
+            #     'no_of_co-pi': no_of_co-pi,
+            #     'funding_agency': funding_agency,
+            #     'sanctioned_date': date,
+            #     'status': status
+            # }
+            'details_of_consultancy_projects': details_of_consultancy_projects, #list of objects
+            ## object
+            # consultancy_project={
+            #     'title': title,
+            #     'amount': amount,
+            #     'role': role,
+            #     'no_of_co-pi': no_of_co-pi,
+            #     'funding_agency': funding_agency,
+            #     'sanctioned_date': date,
+            #     'status': status
+            # }
+            'details_of_outreach_programs': details_of_outreach_programs, #list of objects
+            ## object
+            # outreach_programs={
+            #     'title': title,
+            #     'amount': amount,
+            #     'role': role,
+            #     'no_of_co-pi': no_of_co-pi,
+            #     'funding_agency': funding_agency,
+            #     'sanctioned_date': date,
+            #     'status': status
+            # }
+            'details_of_phd_supervised': details_of_phd_supervised, #list of objects
+            ## object
+            # phd_supervised={
+            #     'sole_guide': sole_guide,
+            #     'joint_guide': joint_guide
+            # }
+            'details_of_phd_supervising': details_of_phd_supervising, #list of objects
+            ## object
+            # phd_supervising={
+            #     'sole_guide': sole_guide,
+            # }
+
+
+            'certificates': certificates, #list of pdfs
+            'photograph': photograph,
+            'signature': signature
+
+
+
+
+}
