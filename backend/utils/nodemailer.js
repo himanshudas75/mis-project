28ff323b3642f9fadb47bb18876d77e7d801dfdc
Himanshu Das <himanshudas75@gmail.com>
2024-04-04 11:27:25 +0530

    removed debug code

--- backend/utils/nodemailer.js
+++ backend/utils/nodemailer.js
@@ -9,11 +9,6 @@ const transporter = nodemailer.createTransport({
     },
 });
 
-console.log(process.env.SMTP_HOST);
-console.log(process.env.SMTP_PORT);
-console.log(process.env.SMTP_USER);
-console.log(process.env.SMTP_PASSWORD);
-
 transporter
     .verify()
     .then(console.log('Connected to SMTP!'))
