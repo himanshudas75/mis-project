fe2a41fa2e48187a88052a1ab098d4f6f326392d
Himanshu Das <himanshudas75@gmail.com>
2024-05-07 23:13:50 +0530

    console

--- frontend/src/hooks/useTickets.js
+++ frontend/src/hooks/useTickets.js
@@ -4,9 +4,11 @@ const useTickets = () => {
     const url = import.meta.env.VITE_TICKET_URL;
     const submitTicket = async (data) => {
         try {
+            console.log('SUBMIT TICKET', data);
             const res = await axios.post(url, JSON.stringify(data));
             return res.data;
         } catch (err) {
+            console.log(err);
             console.log(err?.response?.data);
             return err?.response?.data;
         }
